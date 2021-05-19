8eebbb1a64e0351960fd432efda2731bc2b8b936
SilvinaD <silvinadonvito21@gmail.com>
2021-05-19 20:28:04 -0300

    config heroku

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, "./public")));
 
-app.listen(3030, () => {
+app.listen(process.env.PORT || 3030, () => {
 	console.log("Servidor Corriendo en puerto 3030");
 });
 
